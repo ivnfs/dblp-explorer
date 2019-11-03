0edb0f9b654fbb334be50c0b90f2568101c317f9
Ivan <Ivan@10.0.0.129>
2019-11-03 17:22:29 -0600

    input file

--- dblp/src/dblp/Main.java
+++ dblp/src/dblp/Main.java
@@ -1,10 +1,21 @@
 package dblp;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		File infile = new File("input");
+		
+		BufferedReader read = new BufferedReader(new InputStreamReader(new FileInputStream(infile)));
+		String Line =  read.readLine();
+		while (Line != null) {
+		System.out.println(Line);
+		Line =  read.readLine();
+		}
+		read.close();
 	}
-
 }
